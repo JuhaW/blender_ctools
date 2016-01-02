83c23a6d7cc1047aaae46397bb74eb9d1f63a664
chromoly <chromolytube@gmail.com>
2016-01-02 21:37:48 +0900

    返り値の間違い

--- drawnearest/__init__.py
+++ drawnearest/__init__.py
@@ -1338,7 +1338,7 @@ def find_loop_selection(context, context_dict, bm, mco_region, ring, toggle):
                 context_dict, 'INVOKE_DEFAULT', False,
                 extend=False, deselect=False, toggle=False, ring=False)
     if r == {'CANCELLED'}:
-        return [], [], [], []
+        return None, [], []
 
     verts, edges, faces = get_selected(bm)
 
